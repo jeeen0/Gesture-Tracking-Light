2df5270551bcb0a8f71d103b79276a2278da378f
jeeen0 <102155310+jeeen0@users.noreply.github.com>
2026-05-22 22:32:38 +0900

    Update pi_runtime_config.py

--- src/vision/pi_runtime_config.py
+++ src/vision/pi_runtime_config.py
@@ -128,7 +128,7 @@ SERVO_TILT_SIGN = int(os.environ.get("PI_SERVO_TILT_SIGN", "-1"))
 
 # Pointing calibration. CENTER is the servo angle that points light at camera center.
 SERVO_PAN_CENTER = float(os.environ.get("PI_SERVO_PAN_CENTER", "90.0"))
-SERVO_TILT_CENTER = float(os.environ.get("PI_SERVO_TILT_CENTER", "45.0"))
+SERVO_TILT_CENTER = float(os.environ.get("PI_SERVO_TILT_CENTER", "135.0"))
 
 POINT_PAN_GAIN = float(os.environ.get("PI_POINT_PAN_GAIN", "1.0"))
 POINT_TILT_GAIN = float(os.environ.get("PI_POINT_TILT_GAIN", "1.0"))
